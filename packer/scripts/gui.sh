837a4fbab74684870c9bd2bc2e19e659071fae75
Michael Sleevi <michael.sleevi@gmail.com>
2017-06-17 10:14:46 -0600

    Packer changes to the gui, missed the -y so no gui was installed

--- packer/scripts/gui.sh
+++ packer/scripts/gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
 echo "==> Installing Graphical Interface"
-sudo dnf groupinstall "Development and Creative Workstation" gnome
+sudo dnf -y groupinstall "Development and Creative Workstation" gnome
 echo "==> Setting server to use GUI on startup"
 sudo systemctl set-default graphical.target
